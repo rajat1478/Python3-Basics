6bcb925dac14a3e267c54c7c700af4b52c3230c4
Atul Kumar Jain <atulj968@gmail.com>
2018-08-16 17:12:58 +0530

    Conditional if-elif-else
    Output :
    Welcome to Medical store.

---
+++ ifelif.py
@@ -0,0 +1,8 @@
+store = 'Medical'
+
+if store == 'Mobile':
+  print 'Welcome to mobile store.'
+elif store == 'Medical':
+  print 'Welcome to Medical store.'
+else:
+  print 'Welcome to clothe store.'
