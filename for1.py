6df68e2b01fdeb5138e80f5c702127b4b1a43662
Atul Kumar Jain <atulj968@gmail.com>
2018-08-16 17:37:43 +0530

    Play with for.
    Output:
    (i)2
       4
    (ii)1
        3
        5
    (iii) 15
    (iv) (2,4)
          (6,8)
          (10,12)
    (v)2
        6
        10

---
+++ for1.py
@@ -0,0 +1,25 @@
+# 1
+l=[1,2,3,4,5]
+
+for num in l:
+  if num%2 == 0:
+  print num
+# 2
+for num in l:
+  if num%2 == 1:
+  print num
+ # 3 
+list_sum = 0
+
+for num in l:
+  list_sum = list_sum + num
+  
+print list_sum
+# 4
+l = [(2,4),(6,8),(10,12)]
+
+for item in l:
+  print item
+# 5
+for (t1,t2) in l:
+  print t1
