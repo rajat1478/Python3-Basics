66ed3afba33f4f66ecfdd31e82361e6d815a33a3
Atul Kumar Jain <atulj968@gmail.com>
2018-08-23 00:10:14 +0530

    Method used in Python.
    Output:
    [1,2,3,4,5,6]
    1
    [1,3,4,5,6]
    [1,3,4,5,6,7,8,9]
    [1,2,3,4,5,6,7,8,9]
    [1,2,3,4,5,6,7,8]
    [8,7,6,5,4,3,2,1]

---
+++ methods.py
@@ -0,0 +1,16 @@
+l = [1,2,3,4,5]
+#append add single element in the list.
+l.append(6)
+# count counts that how many time element comes in list
+l.count(3)
+#pop delet the element from the list
+l.pop(1)
+# extend add elements of a list to another.
+y = [7,8,9]
+l.extend(y)
+#insert(index, element) Insert element to the list, index shows location.
+l.insert(1, 2)
+#remove(element) Remove the element from the list
+l.remove(9)
+#reverse Reverse a list.
+l.reverse(l)
