c57daaaf087d7844cf1d2197793676651719efb6
Atul Kumar Jain <atulj968@gmail.com>
2018-08-22 17:41:00 +0530

    While loop used.
    Output:
    X is currently : 0
    x is still less then 10, adding 1 to x
    continuing....
    X is currently : 1
    x is still less then 10, adding 1 to x
    continuing....
    X is currently : 2
    x is still less then 10, adding 1 to x
    Hey x equal 3
    X is currently : 3
    x is still less then 10, adding 1 to x
    continuing....
    X is currently : 4
    x is still less then 10, adding 1 to x
    continuing....
    X is currently : 5
    x is still less then 10, adding 1 to x
    continuing....
    X is currently : 6
    x is still less then 10, adding 1 to x
    continuing....
    X is currently : 7
    x is still less then 10, adding 1 to x
    continuing....
    X is currently : 8
    x is still less then 10, adding 1 to x
    continuing....
    X is currently : 9
    x is still less then 10, adding 1 to x
    continuing....

---
+++ while1.py
@@ -0,0 +1,11 @@
+x=0
+
+while x<10:
+    print "X is currently :",x
+    print "x is still less then 10, adding 1 to x"
+    x += 1
+    if x==3:
+        print "Hey x equal 3"
+    else:
+        print "continuing...."
+        continue
