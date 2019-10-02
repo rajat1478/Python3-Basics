2d2b6fab7e8edf38282a9fb6185ded8818b6f561
Rajat Arya <36148932+rajat1478@users.noreply.github.com>
2019-10-03 03:11:02 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,6 +1,12 @@
-def hello():
-    print("hello")
-    print("hello again")
-hello()
- #again calling
-hello()
+def swap(x, y): 
+    temp = x; 
+    x = y; 
+    y = temp; 
+  
+# Driver code 
+x = 2
+y = 3
+swap(x, y) 
+print(x) 
+print(y)
+
